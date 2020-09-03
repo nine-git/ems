b9ec5631c1c13737f91c857791cf9fe9f20da4db
nine-git <15397725059@163.com>
2020-09-03 18:24:15 +0800

    feat:修改路由文件

--- src/components/MinePost.vue
+++ src/components/MinePost.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <p>我要邮寄办件</p>
+    <div>我的邮寄办件业务</div>
+    <div v-if="postData.length"><p>我要邮寄办件</p></div>
+    <div v-else>你还没有需要邮寄的办件业务</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "MinePost"
+  name: "MinePost",
+  data(){
+    return{
+      postData:[],
+    }
+  },
+  mounted() {
+    //这里请求数据
+    this.postData=[]
+  }
 }
 </script>
 
